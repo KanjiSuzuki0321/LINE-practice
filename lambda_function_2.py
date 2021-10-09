0229b2f42b9d87d0cdbe94169c9f8eac87b74b26
KanjiSuzuki0321 <kanji.suzuki@pyxida.jp>
2021-10-09 18:50:34 +0900

    first commit

---
+++ lambda_function_2.py
@@ -0,0 +1,54 @@
+import os, sys
+from linebot import (LineBotApi, WebhookHandler)
+from linebot.models import (MessageEvent, TextMessage, TextSendMessage,)
+from linebot.exceptions import (LineBotApiError, InvalidSignatureError)
+import logging
+
+#-- new line--
+import boto3
+#-- new line--
+
+logger = logging.getLogger()
+logger.setLevel(logging.ERROR)
+
+channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+
+line_bot_api = LineBotApi(channel_access_token)
+handler = WebhookHandler(channel_secret)
+
+# how does the event look like?
+def lambda_handler(event, context):
+    signature = event["headers"]["x-line-signature"]
+    body = event["body"]
+
+    ok_json = os.environ["ok_json"] # error message as "event" 
+    error_json = os.environ["error_json"]
+
+    @handler.add(MessageEvent, message=TextMessage) 
+    # set function which is run when handler gets specific event "MessageEvent"
+    def message(line_event):
+        text = line_event.message.text
+        line_bot_api.reply_message(line_event.reply_token, TextSendMessage(text=text)) 
+        #-- new line--
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table('kanji-practice')
+        response = table.put_item(
+           Item={
+                'id': line_event.source.userId,
+                'text': line_event.message.text,
+            }
+        )
+        #-- new line--
+        
+
+    try:
+        handler.handle(body, signature)
+    except LineBotApiError as e:
+        logger.error("Got exception from LINE Messaging API: %s\n" % e.message)
+        for m in e.error.details:
+            logger.error("  %s: %s" % (m.property, m.message))
+        return error_json
+    except InvalidSignatureError:
+        return error_json
+    return ok_json 
